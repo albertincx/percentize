a498a5ce98802ed6c88d2265e181374800bc7c99
albertincx <albertincx@gmail.com>
2023-10-08 12:00:07 +0300

    fix min value, README.md

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Description
 ===========
 
-percentize module helps to calculate percent from numbers array
+Percentize helps to calculate percent from numbers array
+with minValue to show. Useful for labels. Avoid zero labels
 
 Install
 =======
@@ -15,11 +16,11 @@ Examples
 ```javascript
 import percentize from 'percentize';
 
-const percentArray = percentize([1,20,20], 3);
-// [3, 48, 48]
+const percentArray = percentize([0.1,20,20]);
+// [1, 49, 49]
 
 const percentArray = percentize([200, 1, 500], 4);
-// [27, 4, 69]
+// [27, 1, 72]
 
 const percentArray = percentize([200, 1, 500]);
 // [28, 1, 71]
--- test/test-percentize.js
+++ test/test-percentize.js
@@ -16,14 +16,15 @@ var group = path.basename(__filename, '.js') + '/';
     {data: [1, 20, 20], expected: [2, 49, 49]},
     {data: [1, 20, 20], min: 1, expected: [2, 49, 49]},
     {data: [1, 20, 20], min: 2, expected: [2, 49, 49]},
-    {data: [1, 20, 20], min: 3, expected: [3, 48, 48]},
-    {data: [0.5, 20, 20], min: 3, expected: [3, 48, 48]},
-    {data: [0.5, 20, 20, 20], min: 3, expected: [3, 32, 32, 32]},
-    {data: [0.5, 20, 20, 20, 20], min: 3, expected: [3, 24, 24, 24, 24]},
-    {data: [0, 20, 5000], min: 3, expected: [3, 97]},
+    {data: [0.1, 20, 20], min: 3, expected: [1, 49, 49]},
+    {data: [2, 2000, 2000], min: 3, expected: [1, 49, 49]},
+    {data: [0.5, 20, 20], min: 3, expected: [1, 49, 49]},
+    {data: [1, 20, 20, 20], min: 3, expected: [1, 33, 33, 33]},
+    {data: [0.5, 20, 20, 20, 20], min: 3, expected: [1, 24, 24, 24, 24]},
+    {data: [0, 20, 5000], min: 3, expected: [1, 99]},
     {data: [0.001, 20, 30, 500], min: 1, expected: [1, 4, 5, 90]},
-    {data: [0.001, 20, 30, 500], min: 3, expected: [3, 4, 5, 88]},
-    {data: [200, 1, 500], min: 4, expected: [27, 4, 69]},
+    {data: [0.001, 20, 30, 500], min: 3, expected: [1, 4, 5, 90]},
+    {data: [200, 1, 500], min: 4, expected: [27, 1, 72]},
     {data: [200, 1, 500], expected: [28, 1, 71]},
     {data: [200, 0, 500], expected: [29, 71]},
 ].forEach(function (v) {
@@ -32,6 +33,6 @@ var group = path.basename(__filename, '.js') + '/';
     var msg = '[' + group + ']: output mismatch.\n'
         + 'Saw: ' + inspect(result) + '\n'
         + 'Expected: ' + inspect(v.expected);
-
+    // console.log(msg);
     assert.deepEqual(result, v.expected, msg);
 });
